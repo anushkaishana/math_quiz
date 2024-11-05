f94e3cec168817c5b56b5b14d32780fef087c637
Anushka Ishana <aisha004@gold.ac.uk>
2024-11-05 11:34:55 +0000

    adding more pages - quiz/register

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const app = express();
 const port = 8000;
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true }));
 
 
 const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
-
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
--- routes/main.js
+++ routes/main.js
@@ -2,11 +2,24 @@
 const express = require('express');
 const router = express.Router();
 
+const data = { quizName: "Educational Quiz App" };
 
 router.get('/', (req, res) => {
-  const data = { quizName: "Educational Quiz App" };
   res.render('index', data);
 });
 
+router.get('/quiz', (req, res) => {
+  res.render('quiz', data);
+});
+
+router.get('/register', (req, res) => {
+  res.render('register', { shopName: "Educational Quiz App" });
+});
+
+
+router.post('/registered', (req, res) => {
+  const { first, last, email } = req.body;
+  res.send(`Hello ${first} ${last}, you are now registered! Confirmation sent to ${email}.`);
+});
 
 module.exports = router;
