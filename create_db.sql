939970d07f44658ccbbbc9165b00031b9908b721
Anushka Ishana <aisha004@gold.ac.uk>
2024-11-16 20:02:11 +0000

    earlier had created databse using remote mysql command line, now using 2 db files within folder and sourcing it remotely

---
+++ create_db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS quiz_app;
+
+USE quiz_app;
+
+CREATE TABLE IF NOT EXISTS questions (
+    -- auto incrementing unique id
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    question TEXT NOT NULL,            
+    options JSON NOT NULL,             
+    answer INT NOT NULL               
+);
+
+
+CREATE USER IF NOT EXISTS 'quiz_app_user'@'localhost' IDENTIFIED BY 'localpassword12';
+GRANT ALL PRIVILEGES ON quiz_app.* TO 'quiz_app_user'@'localhost';
+FLUSH PRIVILEGES;
---
+++ insert_quiz_data.sql
@@ -0,0 +1,6 @@
+USE quiz_app;
+
+INSERT INTO questions (question, options, answer) VALUES
+('What is 3 + 3?', '["5", "6", "7", "8"]', 1),
+('What is 4 * 4?', '["12", "14", "16", "18"]', 2),
+('What is 20 / 4?', '["10", "8", "5", "2"]', 2);
