75f89fe10ee0ef131852bfe58a8aaf72c8922951
Anushka Ishana <aisha004@gold.ac.uk>
2024-11-20 11:52:20 +0000

    adding 2 new databases to store user data and their progress

--- create_db.sql
+++ create_db.sql
@@ -12,7 +12,27 @@ CREATE TABLE IF NOT EXISTS questions (
     answer INT NOT NULL               
 );
 
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_progress (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    level INT DEFAULT 1,
+    -- deletes it if parent row is deleted
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
 CREATE USER IF NOT EXISTS 'quiz_app_user'@'localhost' IDENTIFIED BY 'localpassword12';
 GRANT ALL PRIVILEGES ON quiz_app.* TO 'quiz_app_user'@'localhost';
 FLUSH PRIVILEGES;
+
+
+
+-- References
+-- https://www.geeksforgeeks.org/mysql-deleting-rows-when-there-is-a-foreign-key/ : for deleting when foregin keys 
